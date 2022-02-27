98fe07ccbbea84813d71daae5331e4eeebf8319d
jamiesandell <jamie.sandell@outlook.com>
2022-02-27 07:30:44 +0000

    Working on a bug that is outputting invalid characters, added timer code and commented it out after testing

--- PasswordGenerator.c
+++ PasswordGenerator.c
@@ -5,8 +5,12 @@
 #include <string.h>
 #include <time.h>
 
-#define DEFAULT_PASSWORD_LENGTH 8
+#define DEFAULT_PASSWORD_LENGTH 16
+#define DEFAULT_NUMBER_OF_PASSWORDS_TO_GENERATE 10
+#define MINIMUM_PASSWORD_LENGTH 8
+#define MAXIMUM_PASSWORD_LENGTH 16
 
+long convert_command_line_argument(const char *argument, unsigned int command_line_index);
 char get_random_char(const char *character_set);
 int get_random_number(int start_of_range, int end_of_range);
 
@@ -18,37 +22,25 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
     int password_length = DEFAULT_PASSWORD_LENGTH;
+    long number_of_passwords_to_generate = DEFAULT_NUMBER_OF_PASSWORDS_TO_GENERATE;
     if (argc == 2)
+    {        
+        password_length = convert_command_line_argument(argv[1], 1);
+        if (!password_length)
+        {
+            return EXIT_FAILURE;
+        }
+    }
+    else if (argc == 3)
     {
-        errno = 0;
-        char *end_ptr;
-        long conversion = strtol(argv[1], &end_ptr, 10); //strtol as strtoi has no error checking, as any invalid input is treated as 0
-        /* strtol only sets errno for overflow conditions, not to indicate parsing failures.
-            For that purpose, you have to check the value of the end pointer, but you need to store a pointer to the original string. */
-        if (errno != 0)
+        number_of_passwords_to_generate = convert_command_line_argument(argv[2], 2);
+        if (!number_of_passwords_to_generate)
         {
-            fprintf(stderr, "Error: Converting command line argument %s to long.\n");
             return EXIT_FAILURE;
         }
-        if (end_ptr == argv[1]) // Point to the same starting address as the address of the input then no conversion was done
-        {
-            fprintf(stderr, "Error: No converson of the command line argument %s was performed.\n");
-            return EXIT_FAILURE;
-        }
-        if (*end_ptr != '\0') // If we're not at the null termination then there's some of the input that wasn't converted
-        {
-            fprintf(stderr, "Error: Not all of the command line argument %s was converted to long.\n");
-            return EXIT_FAILURE;
-        }        
-        if (conversion < DEFAULT_PASSWORD_LENGTH || conversion > INT_MAX)
-        {
-            fprintf(stderr, "Error: Command line argument must be between %i and %i but it is %ld\n", DEFAULT_PASSWORD_LENGTH, INT_MAX, conversion);
-            return EXIT_FAILURE;
-        }
-        password_length = conversion;
     }
 
-    char *password = (char *) malloc(password_length + 1); // Allocate the memory for our password, + 1 for null terminating character
+    char *password = (char *) malloc(MAXIMUM_PASSWORD_LENGTH + 1); // Allocate the memory for our password, + 1 for null terminating character
     static const char *const character_sets[] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZ", // https://stackoverflow.com/questions/1200690/c-how-to-correctly-declare-an-array-of-strings
                                                     "abcdefghijklmnopqrstuvwxyz",
                                                     "0123456789",
@@ -58,23 +50,80 @@ int main(int argc, char *argv[])
     
     /* Set the first four characters to the corresponding character set to satisfy, in a basic way,
     that a password must have upper case, lower case, numeric and a special characters */
-    int position = 0;
-    for (; position < number_of_rows; ++position)
+    int position;
+    // Start measuring time
+    /*time_t begin, end;
+    time(&begin);*/
+    for (long password_count = 0; password_count < number_of_passwords_to_generate; ++password_count)
     {
-        *(password + position) = get_random_char(character_sets[position]);
+        position = 0;
+        for (; position < number_of_rows; ++position)
+        {
+            *(password + position) = get_random_char(character_sets[position]);
+        }
+        // set the rest of the password
+        int random_character_set;
+        for (; position < password_length; ++position)
+        {
+            random_character_set = get_random_number(0, number_of_rows - 1);
+            *(password + position) = get_random_char(character_sets[random_character_set]);
+        }
+        *(password + position) = '\0';
+        fprintf(stdout, "%s\n", password);
     }
-    // set the rest of the password
-    int random_character_set;
-    for (; position < password_length; ++position)
-    {
-        random_character_set = get_random_number(0, number_of_rows - 1);
-        *(password + position) = get_random_char(character_sets[random_character_set]);
-    }
-    *(password + position) = '\0';
-    fprintf(stdout, "%s\n", password);
+    // Stop measuring time and calculate the elapsed time
+    // This will measure the wall clock on both Winodws & Linux, but only in full seconds.
+    /*time(&end);
+    time_t elapsed = end - begin;
+    fprintf(stdout, "Generated %ld %i character long passwords in %ld seconds.\n", number_of_passwords_to_generate, password_length, elapsed);*/
     free(password); // Make sure we free what the dynamically allocated
 
     return EXIT_SUCCESS;
+}
+
+long convert_command_line_argument(const char *argument, unsigned int command_line_index)
+{
+    errno = 0;
+    char *end_ptr;
+    long conversion = strtol(argument, &end_ptr, 10); //strtol as strtoi has no error checking, as any invalid input is treated as 0
+    /* strtol only sets errno for overflow conditions, not to indicate parsing failures.
+        For that purpose, you have to check the value of the end pointer, but you need to store a pointer to the original string. */
+    if (errno != 0)
+    {
+        fprintf(stderr, "Error: Converting command line argument %s to long.\n");
+        return 0;
+    }
+    if (end_ptr == argument) // Point to the same starting address as the address of the input then no conversion was done
+    {
+        fprintf(stderr, "Error: No converson of the command line argument %s was performed.\n");
+        return 0;
+    }
+    if (*end_ptr != '\0') // If we're not at the null termination then there's some of the input that wasn't converted
+    {
+        fprintf(stderr, "Error: Not all of the command line argument %s was converted to long.\n");
+        return 0;
+    }
+    switch (command_line_index)
+    {
+        case 1:
+            if (conversion < MINIMUM_PASSWORD_LENGTH || conversion > MAXIMUM_PASSWORD_LENGTH)
+            {
+                fprintf(stderr, "Error: Command line argument 1 must be between %i and %i but it is %ld\n", MINIMUM_PASSWORD_LENGTH, MAXIMUM_PASSWORD_LENGTH, conversion);
+                return 0;
+            }
+            break;
+        case 2:
+            if (conversion < LONG_MIN || conversion > LONG_MAX)
+            {
+                fprintf(stderr, "Error: Command line argument 2 must be between %lf and %ld but it is %ld\n", LONG_MIN, LONG_MAX, conversion);
+                return 0;
+            }
+            break;
+        default:
+            fprintf(stderr, "Error: Unknown command_line_index %u\n", command_line_index);
+            return 0;
+    }    
+    return conversion;
 }
 
 // Gets a random character from the passed in pointer to a character set. The character set must be null terminated.
@@ -90,5 +139,5 @@ char get_random_char(const char *character_set)
 // Gets a random number from start_of_range (inclusive) to end_of_range (inclusive).
 int get_random_number(int start_of_range, int end_of_range)
 {
-    return (rand() % (end_of_range - start_of_range + 1)) + start_of_range;
+    return start_of_range + rand() / (RAND_MAX / (end_of_range - start_of_range + 1) + 1);
 }
